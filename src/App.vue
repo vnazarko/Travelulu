01761259675734d464abc3983320b82a08173d96
vnazarko <alg.vnazarenko@gmail.com>
2023-08-29 15:07:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <h1 class="header__logo">Travelulu</h1>
+
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  /* Header */
+
+  .header {
+    max-width: 100%;
+    padding: 34px 0px;
+    border-bottom: 1px solid #D9DBE9;
+    .container {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+  .header__logo {
+    font-family: Popoins_Bold;
+    font-size: 34px;
+    color: #5F2EEA;
+  }
+</style>
