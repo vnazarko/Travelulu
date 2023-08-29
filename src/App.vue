77c61e0e9c90b185de8a0799e5067c2887422302
vnazarko <alg.vnazarenko@gmail.com>
2023-08-29 16:08:07 +0300

    made fullscreen navigation

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,22 @@
   <header class="header">
     <div class="container">
       <h1 class="header__logo">Travelulu</h1>
-
+      <div id="myNav" class="overlay">
+        <div class="overlay-content">
+            <div class="overlay-content__top">
+              <div class="container">
+                <h1 class="header__logo">Travelulu</h1>
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+              </div>
+            </div>
+          <a href="#" class="overlay__link">Home</a>
+          <a href="#" class="overlay__link">Services</a>
+          <a href="#" class="overlay__link">Contients</a>
+          <a href="#" class="overlay__link">Contact</a>
+          <a href="#" class="overlay__link">Privacy</a>
+        </div>
+      </div>
+      <span style="font-size:30px;cursor:pointer;color:#5F2EEA" onclick="openNav()">&#9776;</span>
     </div>
   </header>
 </template>
@@ -27,5 +42,66 @@
     font-family: Popoins_Bold;
     font-size: 34px;
     color: #5F2EEA;
+  }
+
+  .overlay {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    right: 0;
+    background-color: #2A00A2;
+    overflow-x: hidden;
+    transition: 0.5s;
+  }
+  .overlay-content {
+    position: relative;
+    width: 100%;
+    text-align: left;
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+  }
+  .overlay-content__top {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    padding-bottom: 25px;
+    border-bottom: 1px solid #BCA4FF;
+    .container {
+      display: flex;
+      justify-content: space-between;
+      flex-direction: row;
+    }
+    .header__logo {
+      color: #FCFCFC;
+      margin-top: 15px;
+    }
+  }
+
+  .overlay__link {
+    font-family: Poppins;
+    text-decoration: none;
+    font-size: 30px;
+    color: #F3EFFF;
+    display: block;
+    transition: 0.3s;
+    padding: 40px 19.5%;
+    &:hover {
+      background-color: #5F2EEA;
+    }
+  }
+
+  .overlay a:hover, .overlay a:focus {
+    color: #f1f1f1;
+  }
+
+  .overlay .closebtn {
+    text-decoration: none;
+    font-size: 50px;
+    color: #F3EFFF;
   }
 </style>
