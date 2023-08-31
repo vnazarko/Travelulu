3ec6654fe5925420178fe6930722e6c21484f040
vnazarko <alg.vnazarenko@gmail.com>
2023-08-31 19:29:25 +0300

    made a intro block

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@ export default {
   components: {
     Header,
   },
+  data: () => ({
+    small: false,
+  }),
+  created() {
+    window.addEventListener("resize", this.onResize);
+    this.onResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.onResize);
+  },
+  methods: {
+    onResize() {
+      this.small = window.innerWidth <= 767;
+      console.log("Small display:", this.small);
+    },
+  },
 };
 </script>
 
 <template>
   <Header />
+
   <section class="intro">
+    <img src="./assets/img/intro/boarding.png" alt="" class="intro__bg" v-if="small" />
     <div class="container">
       <div class="intro__text">
         <h1 class="title">Plan your next big trip overseas</h1>
@@ -34,14 +52,14 @@ export default {
   .intro__text {
     position: relative;
     top: 200px;
+    .title {
+      font-family: Poppins_Bold;
+      font-size: 64px;
+      color: #4e4b66;
+      line-height: 66px;
+      width: 669px;
+    }
   }
-}
-.title {
-  font-family: Poppins_Bold;
-  font-size: 64px;
-  color: #4e4b66;
-  line-height: 66px;
-  width: 669px;
 }
 .intro__buttons {
   width: 510px;
@@ -89,6 +107,48 @@ export default {
     border: none;
     color: #f7f7fc;
     transition: all 0.3s;
+  }
+}
+
+@media (max-width: 767px) {
+  .intro {
+    background-image: none;
+    .intro__text {
+      width: 420px;
+      height: 325px;
+      position: relative;
+      top: 43px;
+      display: flex;
+      justify-content: flex-start;
+      flex-direction: column;
+      .title {
+        font-size: 48px;
+        width: 350px;
+      }
+    }
+  }
+  .intro__buttons {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    a {
+      width: 100%;
+    }
+    .intro__btn1 {
+      margin-bottom: 26px;
+    }
+  }
+  .intro__bg {
+    width: 575px;
+    height: 333px;
+    float: right;
+  }
+}
+@media (max-width: 419px) {
+  .intro__bg {
+    width: 400px;
+    height: 280px;
   }
 }
 </style>
